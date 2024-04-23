49672c95f62b8a3ed08be54e41ddd97b4ad6a8d3
Kartik <hellokartikk@gmail.com>
2024-04-23 18:26:54 +0530

    Refactor range slider code

--- range-slider.js
+++ range-slider.js
@@ -1,52 +1,71 @@
-const main = document.querySelector('[tc-range-slider]');
-const input = main.querySelector('input');
+const sliders = document.querySelectorAll('[tc-range-slider]');
 
-const handle = main.querySelector('[range-handle]');
-const track = main.querySelector('[range-track]');
+sliders.forEach((main) => {
+	// const main = document.querySelector('[tc-range-slider]');
+	const input = main.querySelector('input');
 
-const handleWidth = handle.offsetWidth;
+	const handle = main.querySelector('[range-handle]');
+	const track = main.querySelector('[range-track]');
 
-const min = parseInt(main.getAttribute('range-min')) || 0;
-const max = parseInt(main.getAttribute('range-max')) || 100;
+	const handleWidth = handle.offsetWidth;
 
-const rangeSteps = parseInt(main.getAttribute('range-steps'));
+	const min = parseInt(main.getAttribute('range-min')) || 0;
+	const max = parseInt(main.getAttribute('range-max')) || 100;
+	const initialValue = parseInt(input.value) || 0;
 
-let dragging = false;
+	const rangeSteps = parseInt(main.getAttribute('range-steps'));
 
-handle.addEventListener('mousedown', function (e) {
-	dragging = true;
-});
+	let dragging = false;
 
-document.addEventListener('mouseup', function (e) {
-	dragging = false;
-});
+	handle.addEventListener('mousedown', function (e) {
+		dragging = true;
+	});
 
-let value = 20;
+	document.addEventListener('mouseup', function (e) {
+		dragging = false;
+	});
 
-document.addEventListener('mousemove', function (e) {
-	if (!dragging) return;
-	let trackRect = track.getBoundingClientRect();
-	let newLeft = e.clientX - trackRect.left;
-	let rightLimit = track.offsetWidth - handle.offsetWidth;
+	let value = 20;
 
-	if (newLeft < 0) newLeft = 0;
-	if (newLeft > rightLimit) newLeft = rightLimit;
+	document.addEventListener('mousemove', updateHandle);
 
-	let newVal = 0;
+	// updateHandle();
 
-	if (rangeSteps) {
-		newVal = Math.ceil((min + (max - min) * (newLeft / rightLimit)) / rangeSteps) * rangeSteps;
-		if (newVal < max) value = newVal;
-	} else {
-		newVal = min + (max - min) * (newLeft / rightLimit);
-		if (newVal < max) value = newVal;
+	function updateHandle(e) {
+		if (!dragging) return;
+		let clientX = e.clientX;
+		let trackRect = track.getBoundingClientRect();
+		let newLeft = clientX - trackRect.left;
+
+		if (e) newLeft = e.clientX - trackRect.left;
+		// if not an event
+		else newLeft = (trackRect.width * (value - min)) / (max - min);
+		console.log((trackRect.width * (value - min)) / (max - min));
+
+		let rightLimit = track.offsetWidth - handle.offsetWidth;
+
+		if (newLeft < 0) newLeft = 0;
+		if (newLeft > rightLimit) newLeft = rightLimit;
+
+		let newVal = 0;
+
+		if (rangeSteps) {
+			newVal =
+				Math.ceil(
+					(min + (max - min) * (newLeft / rightLimit)) / rangeSteps
+				) * rangeSteps;
+			if (newVal < max) value = newVal;
+		} else {
+			newVal = min + (max - min) * (newLeft / rightLimit);
+			if (newVal < max) value = newVal;
+		}
+
+		const handleLeft = ((newVal - min) / (max - min)) * 100;
+
+		if (handleLeft === 0) handle.style.left = '0px';
+		else handle.style.left = `calc(${handleLeft}% - ${handleWidth}px)`;
+
+		input.value = value;
+		input.dispatchEvent(new Event('change'));
 	}
-
-	const handleLeft = ((newVal - min) / (max - min)) * 100;
-
-	if (handleLeft === 0) handle.style.left = '0px';
-	else handle.style.left = `calc(${handleLeft}% - ${handleWidth}px)`;
-
-	input.value = value;
-	input.dispatchEvent(new Event('change'));
 });
